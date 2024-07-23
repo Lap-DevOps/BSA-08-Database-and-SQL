85222a29578e5e788dc90c10484370f443d079dc
Andrii L <lap.devops@gmail.com>
2024-07-23 18:40:36 +0100

    BSA08 * fix condition fot tables creation with timestamp

---
+++ init-db/01-create-user-tables.sql
@@ -0,0 +1,27 @@
+create TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(100) NOT NULL,
+    avatar_file_id INTEGER
+);
+
+create TABLE files (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    file_name VARCHAR(255) NOT NULL,
+    mime_type VARCHAR(100) NOT NULL,
+    file_key VARCHAR(255) NOT NULL UNIQUE,
+    url VARCHAR(255) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+create TABLE user_files (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    file_id INTEGER NOT NULL
+);
+
+
