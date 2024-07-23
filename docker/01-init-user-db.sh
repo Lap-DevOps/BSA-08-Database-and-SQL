30746febeef0ac8512db0788dfd1adf3d5f57f84
Andrii L <lap.devops@gmail.com>
2024-07-23 14:11:02 +0100

    init project

---
+++ docker/01-init-user-db.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+set -e
+echo "** Creating  user and config"
+
+if [[ -z "$POSTGRES_USER_DEV" || -z "$POSTGRES_PASSWORD_DEV" || -z "$POSTGRES_DB_DEV" ]]; then
+  echo "Error: POSTGRES_USER, POSTGRES_PASSWORD, and POSTGRES_DB must be set"
+  exit 1
+fi
+
+
+psql -v ON_ERROR_STOP=1 -U postgres  -d postgres <<-EOSQL
+
+  CREATE USER ${POSTGRES_USER_DEV} NOSUPERUSER;
+  ALTER USER ${POSTGRES_USER_DEV} WITH PASSWORD '${POSTGRES_PASSWORD_DEV}';
+  ALTER ROLE ${POSTGRES_USER_DEV} SET client_encoding TO 'utf8';
+  ALTER ROLE ${POSTGRES_USER_DEV} SET default_transaction_isolation TO 'read committed';
+  ALTER ROLE ${POSTGRES_USER_DEV} SET timezone TO 'UTC';
+
+  CREATE DATABASE ${POSTGRES_DB_DEV} WITH
+    ENCODING 'UTF8'
+    OWNER = ${POSTGRES_USER_DEV}
+    LC_COLLATE = 'en_US.utf8'
+    LC_CTYPE = 'en_US.utf8';
+
+  GRANT ALL PRIVILEGES ON DATABASE ${POSTGRES_DB_DEV} TO ${POSTGRES_USER_DEV};
+
+EOSQL
+
+echo "** Done"
---
+++ docker/02-entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+export PGPASSWORD=$POSTGRES_PASSWORD_DEV
+
+until psql -U "$POSTGRES_USER_DEV" -d "$POSTGRES_DB_DEV" -c '\q'; do
+  >&2 echo "Postgres is still unavailable - sleeping"
+  sleep 1
+done
+
+
+for f in /init-db/*; do
+  case "$f" in
+    *.sh)  echo "$0: running $f"; . "$f" ;;
+    *.sql) echo "$0: running $f"; psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER_DEV" --dbname "$POSTGRES_DB_DEV" -f "$f"; ;;
+    *)     echo "$0: ignoring $f" ;;
+  esac
+  echo
+done
+
+exec "$@"
+
